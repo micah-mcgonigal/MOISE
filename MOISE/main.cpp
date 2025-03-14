53f0ac67c88d7d388f483ed1c356c42c9de0faf6
Micah McGonigal <micah.mcgonigal097@gmail.com>
2025-03-14 10:40:11 -0400

    Started working on add json import support.

--- MOISE/main.cpp
+++ MOISE/main.cpp
@@ -3,7 +3,11 @@
 #include "pch.h"
 #include "Moise_data.h"
 #include "MoiseLibrary.h"
+#include "json.hpp"
+#include <string>
 #include <iostream>
+
+using json = nlohmann::json;
 
 double currentTime;
 double timeAdvance;
@@ -21,6 +25,12 @@ void Init(int setSampleRate) {
 	sampleRate = setSampleRate;
 	timeAdvance = 1.0 / sampleRate;
 	currentTime = 0;
+}
+
+int LoadPackage(std::string jsonString) {
+	json data = json::parse(jsonString);
+
+	track testTrack = data.template get<track>();
 }
 
 void FillWaveformData(float data[], int sampleTotal, int channels) {
