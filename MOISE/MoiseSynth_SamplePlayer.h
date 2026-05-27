341bc4a41a9878fb090d30c75e1606c54c4252a2
Micah McGonigal <44176450+micah-mcgonigal@users.noreply.github.com>
2026-05-27 11:35:22 -0400

    Implemented Hermite interpolation for sample playback.

--- MOISE/MoiseSynth_SamplePlayer.h
+++ MOISE/MoiseSynth_SamplePlayer.h
@@ -64,7 +64,7 @@ private:
 
 public:
 	int activeSampleId = 0; //The ID of the sample to use for when sample banks are in use. Currently this corresponds to an index into the sample bank.
-	bool hasPlayedFirstNoteOn = false; //Whether or not we've played our first NoteOn()
+	bool startedLoopingSample = false; //True once a playing sample has started looping
 
 	/// <summary>
 	/// Default constructor for a sample player.
@@ -122,7 +122,6 @@ public:
 		sampleRate = setSampleRate;
 		channels = setChannels;
 		initialized = true;
-		hasPlayedFirstNoteOn = false;
 	}
 
 	/// <summary>
@@ -147,6 +146,67 @@ public:
 	}
 
 	/// <summary>
+	/// Resampling interpolation. Source: https://stackoverflow.com/questions/1125666/how-do-you-do-bicubic-or-other-non-linear-interpolation-of-re-sampled-audio-da
+	/// More info: https://stackoverflow.com/questions/5390957/explanation-of-interpolate-hermite-method
+	/// </summary>
+	/// <param name="x0">Sample N - 1</param>
+	/// <param name="x1">Sample N</param>
+	/// <param name="x2">Sample N + 1</param>
+	/// <param name="x3">Sample N + 2</param>
+	/// <param name="t">Time between samples (0-1)</param>
+	/// <returns></returns>
+	float InterpolateHermite4pt3oX(float x0, float x1, float x2, float x3, float t) {
+		float c0 = x1;
+		float c1 = .5F * (x2 - x0);
+		float c2 = x0 - (2.5F * x1) + (2 * x2) - (.5F * x3);
+		float c3 = (.5F * (x3 - x0)) + (1.5F * (x1 - x2));
+		return (((((c3 * t) + c2) * t) + c1) * t) + c0;
+	}
+
+	int GetAdjustedSamplePosition(const MOISE_SamplePlayer_Sample &sampleData, int position) {
+		if (startedLoopingSample) {
+			if (position < sampleData.loopStart) {
+				position += (sampleData.loopEnd - sampleData.loopStart);
+			} else if (position >= sampleData.loopEnd) {
+				position -= (sampleData.loopEnd - sampleData.loopStart);
+			}
+		}
+
+		while (position < 0) {
+			position += sampleData.waveformLength;
+		}
+
+		while (position >= sampleData.waveformLength) {
+			position -= sampleData.waveformLength;
+		}
+
+		return position;
+	}
+
+	float GetInterpolatedSampleAtCurrentPosition(const MOISE_SamplePlayer_Sample &sampleData) {
+
+		int samplePosition[4];
+
+		//Get the current base sample position for interpolation by flooring the current precise position (with modulo to ensure position is within sample waveform bounds).
+		int baseSamplePosition = static_cast<int>(std::floor(currentPosition)) % sampleData.waveform.size();
+		baseSamplePosition = max(1, baseSamplePosition);
+
+		for (int i = -1; i < 3; i++) {
+			samplePosition[i + 1] = GetAdjustedSamplePosition(sampleData, baseSamplePosition + i);
+		}
+
+		if (samplePosition[0] >= sampleData.loopStart) {
+			startedLoopingSample = true;
+		}
+
+		return InterpolateHermite4pt3oX(sampleData.waveform[samplePosition[0]],
+			sampleData.waveform[samplePosition[1]],
+			sampleData.waveform[samplePosition[2]],
+			sampleData.waveform[samplePosition[3]],
+			currentPosition - baseSamplePosition);
+	}
+
+	/// <summary>
 	/// Get the next sample based on amount of time to advance.
 	/// </summary>
 	/// <param name="timeAdvance">The amount of time to advance.</param>
@@ -163,11 +223,8 @@ public:
 			//Get active sample data
 			const MOISE_SamplePlayer_Sample &sampleData = sampleBank[activeSampleId];
 
-			//Get the current sample position by flooring the current precise position (with modulo to ensure position is within sample waveform bounds).
-			currentSamplePosition = static_cast<int>(std::floor(currentPosition)) % sampleData.waveform.size();
-
 			//Get a sample from the waveform data at the currentSamplePosition.
-			float centerSample = sampleData.waveform[currentSamplePosition];
+			float centerSample = GetInterpolatedSampleAtCurrentPosition(sampleData);
 
 			//Multiply the sample by the current envelope value
 			centerSample *= currentEnvelope.currentValue;
@@ -215,17 +272,7 @@ public:
 		playing = true;
 		currentEnvelope.state = EnvelopeState::Attacking;
 		currentEnvelope.currentValue = 0;
-
-		//In the future, I will select the sample to use based on the value, as there can be multiple samples in a bank.
-		//Refer to old MOISE Unity project.
-
-		// For now, for testing, cycle through the samples in the bank after the first NoteOn() (so that we still start with the first sample)
-		if (hasPlayedFirstNoteOn) {
-			activeSampleId = (activeSampleId + 1) % sampleBank.size();
-		}
-		else {
-			hasPlayedFirstNoteOn = true;
-		}
+		startedLoopingSample = false;
 	}
 };
 
